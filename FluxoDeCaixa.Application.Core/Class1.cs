c41027bfbb13f1e8cc1c69c4168370e29e579c2e
Marcelo Dias <mardsystems@gmail.com>
2020-09-19 18:47:16 -0300

    task: limpa projetos de Aplicação

--- FluxoDeCaixa.Application.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace FluxoDeCaixa.Application.Core
-{
-    public class Class1
-    {
-    }
-}
--- FluxoDeCaixa.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace FluxoDeCaixa.Application
-{
-    public class Class1
-    {
-    }
-}
