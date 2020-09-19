fd720a7cb2d31cefbaf784ba02a8c5f7f40d76f8
Marcelo Dias <mardsystems@gmail.com>
2020-09-19 18:46:52 -0300

    task: adiciona projetos de Aplicação

---
+++ FluxoDeCaixa.Application.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FluxoDeCaixa.Application.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ FluxoDeCaixa.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FluxoDeCaixa.Application
+{
+    public class Class1
+    {
+    }
+}
